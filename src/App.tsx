a335d449d3265c598e194ed7c1d020eb2c2a2456
sartxi <sean.archibeque@gmail.com>
2022-03-07 11:47:20 -0700

    sass and custom styles

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,9 @@ import "@ionic/react/css/display.css";
 
 /* Theme variables */
 import "./theme/variables.css";
-import "./theme/custom.css";
+
+/* Custom Styles */
+import "./theme/custom.scss";
 
 setupIonicReact();
 
